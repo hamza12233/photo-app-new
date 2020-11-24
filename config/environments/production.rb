df3fef22397a8c8757bedbd80ec5bcb2f4a015dd
Hamza Saleem <hafizhamzasaleem4@gmail.com>
2020-11-24 22:21:41 +0500

    issues are fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,32 +11,11 @@ Rails.application.configure do
   config.eager_load = true
 
 
+
+
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options ={:host => 'photo-app-newone.herokuapp.com', :protocol => 'https'}
-
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => Rails.application.credentials.dig(:user_name),
-    :password => Rails.application.credentials.dig(:password),
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-
-#   ActionMailer::Base.smtp_settings = {
-#     :user_name => ENV['SENDGRID_USERNAME'],
-#     :password => ENV['SENDGRID_PASSWORD'],
-#     :domain => 'heroku.com',
-#     :address => 'smtp.sendgrid.net',
-#     :port => '587',
-#     :authentication => :plain,
-#     :enable_starttls_auto => true
-# }
-
-#   config.action_mailer.delivery_method = :smtp
-#   config.action_mailer.default_url_options = { :host => 'photo-app-newone.herokuapp.com', :protocol=> 'https' }
+  config.action_mailer.default_url_options = { :host => 'photo-app-newone.herokuapp.com', :protocol=> 'https' }
 
 
 
@@ -139,4 +118,15 @@ config.action_mailer.default_url_options ={:host => 'photo-app-newone.herokuapp.
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+}
+
 end
