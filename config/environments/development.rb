d0a85dc2fb38b551d6577c16dec5410b026a099c
Hamza Saleem <hafizhamzasaleem4@gmail.com>
2020-12-02 22:15:20 +0500

    mmmm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,8 +10,14 @@ Rails.application.configure do
   config.eager_load = false
 
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options ={:host => 'http://localhost:3000'}
+
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options ={:host => 'http://localhost:3000'}
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 
   
 
